cbb60789e2215ec3a467557cdcf24fd3fc743d41
vinabdst <alvinabudiastuti@gmail.com>
2025-05-13 21:06:35 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,12 @@
 <?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "rental_ip";
 
+    $conn = mysqli_connect($host, $user, $password, $db); //buat konek ke db
+
+    if ($conn->connect_error) {
+        die('Maaf, koneksi gagal: ' . $conn->connect_error);
+    }
 ?>
