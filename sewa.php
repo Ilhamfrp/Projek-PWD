dc097ac112140165de9a45887e1e61f15b3c2ca3
vinabdst <alvinabudiastuti@gmail.com>
2025-05-14 21:54:48 +0700

    Update sewa.php

--- sewa.php
+++ sewa.php
@@ -1,2 +1,12 @@
 <?php
+    include 'koneksi.php';
+    session_start();
+
+    if (!isset($_SESSION["login"])){
+        header("Location: login.php");
+        exit;
+    }
+
+    //ngambil dr table
+    $query = mysqli_query($conn, "SELECT * FROM users");
 ?>
